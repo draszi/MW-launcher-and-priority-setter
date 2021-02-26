0aa724038975f90073c037b198ebf78f7d498927
Arbi <drazhi@live.ca>
2021-02-26 12:51:03 -0500

    added all files

---
+++ mw&priority.bat
@@ -0,0 +1,3 @@
+@ECHO OFF
+START "Modern Warfare Launcher.exe" "complete-file-path\Call of Duty Modern Warfare\Modern Warfare Launcher.exe" 
+START "priority-setter-AN.exe" "complete-file-path\priority-setter-AN.exe"
---
+++ priority-setter.cs
@@ -0,0 +1,36 @@
+﻿using System;
+
+using System.Diagnostics;
+
+namespace ConsoleApp3
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            for (;;)
+            {
+                Process[] pname = Process.GetProcessesByName("ModernWarfare");
+
+                if (pname.Length == 0)
+                {
+                    Console.WriteLine("ModernWarfare not running");
+                    System.Threading.Thread.Sleep(10000);
+                }
+
+                else
+                {
+                    Console.WriteLine("ModernWarfare running, checking priority");
+                    foreach (var process in Process.GetProcessesByName("ModernWarfare"))
+                    {
+                        //Default priority is "AboveNormal" but it can be changed to "Normal" as well
+                        process.PriorityClass = ProcessPriorityClass.AboveNormal;
+						Console.WriteLine("ModernWarfare priority set to AboveNormal");
+						System.Threading.Thread.Sleep(10000);
+						System.Environment.Exit(1);
+                    }
+                }
+            }
+        }
+    }
+}
